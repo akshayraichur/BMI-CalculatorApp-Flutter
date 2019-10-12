c512c40f3bd114cd953fcaa7bb219492ba06fbca
Akshay Raichur <akshay.raichur99@gmail.com>
2019-10-12 22:36:06 +0530

    Create constants.dart

---
+++ lib/constants.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+const inActiveCardColor = Color(0xFF111328);
+const activeCardColor = Color(0xFF1D1E33);
+
+const labelStyle = TextStyle(color: Color(0xFF8D8E98), fontSize: 18.0);
+
+const klabelTextStyles = TextStyle(fontSize: 50.0, fontWeight: FontWeight.w900);
+
+const kLargeButtonText = TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold);
+
+const kTitleTextStyle = TextStyle(fontSize: 50.0, fontWeight: FontWeight.bold);
+
+const kResultTextStyle = TextStyle(
+    color: Color(0xFF24D876), fontSize: 20.0, fontWeight: FontWeight.bold);
+
+const kBmiAns = TextStyle(fontSize: 40.0, fontWeight: FontWeight.bold);
