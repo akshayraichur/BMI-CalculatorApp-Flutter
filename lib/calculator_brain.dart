681acb4fdcba92fb6a5deeb45f609934ce978303
Akshay Raichur <akshay.raichur99@gmail.com>
2019-10-12 22:36:11 +0530

    Create calculator_brain.dart

---
+++ lib/calculator_brain.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'dart:math';
+
+class calculator_brain {
+  calculator_brain({this.height, this.weight});
+
+  final int height;
+  final int weight;
+
+  double _bmi;
+
+  String CalculateBMI() {
+    _bmi = weight / pow(height / 100, 2);
+    return _bmi.toStringAsFixed(1);
+  }
+
+  String getResult() {
+    if (_bmi >= 25) {
+      return 'Overweight';
+    } else if (_bmi > 18.5) {
+      return 'Normal';
+    } else
+      return 'Under-weight';
+  }
+
+  String getInterpretation() {
+    if (_bmi >= 25) {
+      return 'You have a higher than normal body weight. Try to exercise more';
+    } else if (_bmi > 18.5) {
+      return 'You have a normal body weight, good job';
+    } else
+      return 'You have a lower than normal body weight. Try to gain weight';
+  }
+}
